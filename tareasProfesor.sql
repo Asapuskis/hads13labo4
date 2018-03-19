71079f58d62dd46e83526e1484eb5fb28ba50181
Asapuskis <aguayoasier@gmail.com>
2018-03-19 19:40:03 +0100

    labo4

---
+++ tareasProfesor.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE tareasProfesor
+	@email nvarchar(100),
+	@codigoAsig nvarchar(10)
+AS
+	SELECT TareasGenericas.Codigo, TareasGenericas.Descripcion, TareasGenericas.HEstimadas, TareasGenericas.Explotacion, TareasGenericas.TipoTarea 
+	FROM TareasGenericas
+	INNER JOIN GruposClase ON TareasGenericas.codAsig = GruposClase.codigoAsig
+	INNER JOIN ProfesoresGrupo ON GruposClase.codigo = ProfesoresGrupo.codigoGrupo
+	WHERE GruposClase.CodigoAsig = @codigoAsig AND ProfesoresGrupo.email = @email
